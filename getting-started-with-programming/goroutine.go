ce5206f4b027c8a7c53882e0bd71c7234f21eb96
hyoukam <838611931@qq.com>
2022-05-05 14:29:24 +0800

    feat(goroutine) 协程

---
+++ getting-started-with-programming/goroutine.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"time"
+)
+
+func SayGreetings(greeting string, times int) {
+	// 循环
+	for i := 0; i < times; i++ {
+		log.Println(greeting)
+		d := time.Second * time.Duration(rand.Intn(5)) / 2
+		time.Sleep(d) // 睡眠片刻（随机0到2.5秒）
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	log.SetFlags(0)
+	go SayGreetings("hi!", 10)
+	go SayGreetings("hello!", 10)
+	time.Sleep(2 * time.Second)
+}
