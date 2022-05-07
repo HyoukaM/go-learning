a82763f81c8b2ab072faeec1717fb20dacae8226
hyoukam <838611931@qq.com>
2022-05-07 14:22:40 +0800

    feat(function_unit_test): 函数单元测试

---
+++ function/function_unit_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+/**
+类型	格式	作用
+测试函数	函数名前缀为Test	测试程序的一些逻辑行为是否正确
+基准函数	函数名前缀为Benchmark	测试函数的性能
+示例函数	函数名前缀为Example	为文档提供示例文档
+*/
+/**
+	1、文件名必须以xx_test.go命名
+    2、方法必须是Test[^a-z]开头
+    3、方法参数必须 t *testing.T
+    4、使用go test执行单元测试
+*/
+func main() {
+
+}
+
+// 分割字符
+func Split(s, sep string) (result []string) {
+	i := strings.Index(s, sep)
+
+	for i > -1 {
+		result = append(result, s[:i])
+		s = s[i+1:]
+		i = strings.Index(s, sep)
+	}
+	result = append(result, s)
+	return
+}
+func TestSplit(t *testing.T) { // 测试函数名必须以Test开头，必须接收一个*testing.T类型参数
+	got := Split("a:b:c", ":")         // 程序输出的结果
+	want := []string{"a", "b", "c"}    // 期望的结果
+	if !reflect.DeepEqual(want, got) { // 因为slice不能比较直接，借助反射包中的方法比较
+		t.Errorf("excepted:%v, got:%v", want, got) // 测试失败输出错误提示
+	}
+}
